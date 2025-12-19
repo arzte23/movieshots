881189497deccbbf93e3fefb4ea24742ad4e5c4f
Vitaliy <ryashentsev.vitaliy@gmail.com>
2025-12-19 11:22:54 +0300

    Add urls, views and templates for home and detail pages

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,10 +19,11 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("gallery.urls")),
 ] + debug_toolbar_urls()
 
 if settings.DEBUG:
--- gallery/views.py
+++ gallery/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+from django.shortcuts import get_object_or_404, render
+
+from .models import MediaItem, Screenshot
+
+
+def home(request):
+    recent_screenshots = Screenshot.objects.select_related("media_item").order_by(
+        "-created_at"
+    )[:20]
+
+    return render(request, "gallery/home.html", {"screenshots": recent_screenshots})
+
+
+def media_detail(request, slug):
+    item = get_object_or_404(MediaItem, slug=slug)
+    images = item.screenshots.all()
+
+    return render(
+        request, "gallery/media_detail.html", {"item": item, "images": images}
+    )
