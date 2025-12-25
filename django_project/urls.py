f5dd9ace5b2e187f638da02cd63143ab5e736a69
Vitaliy <ryashentsev.vitaliy@gmail.com>
2025-12-25 09:09:14 +0300

    Fix media serving in production

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,14 +17,21 @@ Including another URLconf
 
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("gallery.urls")),
 ] + debug_toolbar_urls()
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {
+            "document_root": settings.MEDIA_ROOT,
+        },
+    ),
+]
