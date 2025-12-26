3450ba2f5cf6d93e29c9c9c1bcd98a0cb2e094b5
Vitaliy <ryashentsev.vitaliy@gmail.com>
2025-12-26 09:49:44 +0300

    Update settings (add WHITENOISE_MANIFEST_STRICT = False)

--- django_project/settings.py
+++ django_project/settings.py
@@ -164,3 +164,5 @@ CLOUDINARY_STORAGE = {
     "API_KEY": env.str("CLOUDINARY_API_KEY", default=""),
     "API_SECRET": env.str("CLOUDINARY_API_SECRET", default=""),
 }
+
+WHITENOISE_MANIFEST_STRICT = False
