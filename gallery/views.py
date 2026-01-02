8e2908810b5eb1b55edaea5f6f308d7241b77bc3
Vitaliy <ryashentsev.vitaliy@gmail.com>
2026-01-02 10:12:26 +0300

    add AJAX for like button

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Q
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import MediaItem, Screenshot
@@ -70,8 +71,15 @@ def toggle_favorite(request, pk):
 
     if request.user in screenshot.favorites.all():
         screenshot.favorites.remove(request.user)
+        is_favorite = False
     else:
         screenshot.favorites.add(request.user)
+        is_favorite = True
+
+    if request.headers.get("x-requested-with") == "XMLHttpRequest" or request.accepts(
+        "application/json"
+    ):
+        return JsonResponse({"is_favorite": is_favorite})
 
     return redirect(request.META.get("HTTP_REFERER", "home"))
 
