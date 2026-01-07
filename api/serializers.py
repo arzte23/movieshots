51567d7b13f8ca782cba584d66fc7ece73158816
Vitaliy <ryashentsev.vitaliy@gmail.com>
2026-01-07 10:54:16 +0300

    fix tags field in ScreenshotSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,15 +1,20 @@
 from rest_framework import serializers
+from taggit.serializers import TaggitSerializer, TagListSerializerField
 
 from gallery.models import MediaItem, Screenshot
 
 
-class ScreenshotSerializer(serializers.ModelSerializer):
+class ScreenshotSerializer(TaggitSerializer, serializers.ModelSerializer):
+    tags = TagListSerializerField()
+
     class Meta:
         model = Screenshot
         fields = ["id", "image", "tags"]
 
 
 class MediaItemSerializer(serializers.ModelSerializer):
+    screenshots = ScreenshotSerializer(many=True, read_only=True)
+
     class Meta:
         model = MediaItem
         fields = [
